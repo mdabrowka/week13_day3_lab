39e3dbe2d7153a767aa0d80bca56ee591206eb83
Marta <martadabrowka@gmail.com>
2017-12-20 13:17:25 +0000

    Post request working

---
+++ client/src/models/countries.js
@@ -0,0 +1,4 @@
+
+
+// 
+// const url = "https://restcountries.eu/rest/v2/all";
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const parser = require('body-parser');
+const server = express();
+const MongoClient = require('mongodb').MongoClient;
+
+
+server.use(parser.json());
+server.use(express.static('client/build'));
+server.use(parser.urlencoded({extended: true}));
+
+MongoClient.connect('mongodb://localhost:27017', function(err, client){
+  if(err) {
+    console.log(err);
+    return;
+  }
+  const db = client.db("bucketlist");
+
+  console.log("connect to database");
+
+server.post('/api/countries', function(req, res) {
+  db.collection('countries').insert(req.body, function(err, result) {
+    if(err) {
+      console.log(err);
+      res.status(500);
+      res.send();
+      return;
+    }
+    console.log('Saved to database');
+    res.status(201);
+    res.json(result.ops[0]);
+  });
+});
+
+server.listen(3000, function() {
+  console.log("listening on port 3000");
+});
+
+});
